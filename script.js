c063c5f744a9f91bba126af0dd749d0cf058f9fc
typing-xii <118524461+typing-xii@users.noreply.github.com>
2025-09-30 15:51:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+const a= 5;
+const b=10;
+
+result = a+b;
+console.log(result);
+
+let text = "coding";
+let num = "10";
+let rnum = 10;
+console.log(typeof(text));
+console.log(typeof(num));
+console.log(typeof(num));
+
+console.log(text,num);
+
+console.log(typeof(text+num));
+
+
+let fruits = ['mango','apple','guava'];
+console.log(fruits)
+fruits.push('grapes');
+console.log("Total Fruits",fruits.length)
+
+console.log(fruits);
+
+let student = {
+    Name: "WEB DEV",
+    Age:20,
+    Couse:"Computer Science",
+    isEnrolled: true
+}
+
+console.log(student);
+console.log(student.Name);
+
+
+//arithmatic operators
+let x = 2;
+console.log("Addition : x + 3 =",x+3);
+console.log("Subtraction : x - 3 =",x-3);
+console.log("Multiplication: x * 3 =",x*5)
+console.log("Power : x ** 3 = ",x**3)
+
+//
+console.log("Increment ++ x ++ 3 =",++x)
+console.log("Decrement -- x -- 3 =",--x)
+
+//
+console.log("Division x / 3 =",x/3)
+console.log("Modulus x $ 3 =",x%3)
+
+let y = 2;
+y++;
+z=y;
+
+console.log(y);
+console.log(z);
+
